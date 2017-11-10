a62c674e67bc24f05038e8473a70c19615e34618
chitboon <chitboon@gmail.com>
2017-11-10 08:00:47 +0800

    Added Tutorial 2

--- practical3/Quiz.py
+++ practical3/Quiz.py
@@ -1,6 +1,7 @@
 import random
 
 class Question:
+    question_list = []
     def __init__(self, question, option):
         self.question = question
         self.answer = option
@@ -13,27 +14,28 @@ class Quiz:
 
     # this method creates 10 random questions and stored in questions class attribute
     def create_question(self):
-        q = Question('A bird can fly', True)
+        q = Question('A bird can fly', 'Y')
         self.__class__.questions.append(q)
-        q1 = Question('A dog can fly', False)
+        q1 = Question('A dog can fly', 'N')
         self.__class__.questions.append(q1)
-        q2 = Question('A cow can fly', False)
+        q2 = Question('A cow can fly', 'N')
         self.__class__.questions.append(q2)
-        q3 = Question('A bee can fly', True)
+        q3 = Question('A bee can fly', 'Y')
         self.__class__.questions.append(q3)
-        q4 = Question('A fish can swim', True)
+        q4 = Question('A fish can swim', 'Y')
         self.__class__.questions.append(q4)
 
-quiz = Quiz()
-score = 0
+    def run(self, count):
+        score = 0
+        for i in range(count):
+            num = random.randint(0, 5)
+            q = Quiz.questions[num]
+            answer = input(q.question + '(Y or N): ')
+            if answer.upper() == q.answer:
+                score += 1
+                print('Yes, you got it right, your score is', score)
+            else:
+                print('Sorry, you got it wrong, your score is', score)
 
-for i in range(3):
-    num = random.randint(0,5)
-    q = Quiz.questions[num]
-    answer = input(q.question + '(Y or N): ')
-    if answer.upper() == 'Y' and q.answer :
-        score += 1
-        print('Yes, you got it right, your score is', score)
-    else:
-        print('Sorry, you got it wrong, your score is', score)
-
+q = Quiz()
+q.run(3)
