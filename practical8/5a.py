5c4f9bd8f253f19e1332778eb9baa6748bed3a9d
chitboon <chitboon@gmail.com>
2017-12-04 10:05:10 +0800

    Added code template for Question 5, Practical 8

---
+++ practical8/5a.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.math = 0
+        self.chinese = 0
+        self.english = 0
+        self.science = 0
+        self.choices = []
+
+    def get_score(self):
+        return (self.math + self.chinese + self.english + self.science) / 4
+
+def load_result():
+    students = []
+    # implement the load result logic here
+    return students
+
+list = load_result()
+for s in list:
+    print(s.name, s.get_score())
